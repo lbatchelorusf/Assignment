cf45acf1321f8a280dece62e8796a274ef492649
lbatchelorusf <lbatchelor@usf.edu>
2025-11-23 20:44:28 -0500

    weekly hours print

--- app.py
+++ app.py
@@ -2,3 +2,5 @@ print("Welcome to my Python Program!")
 
 hours = input("How many hours did you study today?")
 hours = float(hours)
+weekly_hours = hours * 7
+print(f"You studied {weekly_hours} hours this week")
