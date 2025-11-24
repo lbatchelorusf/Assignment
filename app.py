6d9bcf4e0f4bdd1bbec86addb0410b8ec61d79b1
lbatchelorusf <lbatchelor@usf.edu>
2025-11-23 20:47:26 -0500

    try and except

--- app.py
+++ app.py
@@ -4,3 +4,8 @@ hours = input("How many hours did you study today?")
 hours = float(hours)
 weekly_hours = hours * 7
 print(f"You studied {weekly_hours} hours this week")
+try:
+    hours = float(hours)
+except ValueError:
+    print("Please enter a valid number for hours studied.")
+    exit()
